6c46fd1494eaee9d5911b9e6d55e9f7ae645d356
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-09-09 18:12:36 +0530

    Add. css styles

--- script.js
+++ script.js
@@ -6,22 +6,30 @@ const sendChatBtn = document.querySelector(".chat-input #send-btn");
 const maximiseBtn = document.querySelector(".maximise-btn");
 const minimiseBtn = document.querySelector(".minimise-btn");
 
-
 let userMessage = null;
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
-    // Create a chat <li> element with passed message and className
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", `${className}`);
-    const chatP = document.createElement("p");
-    chatP.textContent = message;
-    chatLi.appendChild(chatP);
-    return chatLi; // return chat <li> element
+
+    if (className === "outgoing") {
+        const chatP = document.createElement("p");
+        chatP.textContent = message;
+        chatLi.appendChild(chatP);
+    } else {
+        const imgTag = `<img src="botimg.svg" class="chat-robot"/>`;
+        const chatP = document.createElement("p");
+        chatP.textContent = message;  
+        chatLi.innerHTML = imgTag;
+        chatLi.appendChild(chatP);
+    }
+    
+    return chatLi;
 }
 
 const handleChat = () => {
-    userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
+    userMessage = chatInput.value; // Get user entered message and remove extra whitespace
     if(!userMessage) return;
 
     // Clear the input textarea and set its height to default
@@ -37,7 +45,7 @@ const handleChat = () => {
         const incomingChatLi = createChatLi("Thinking...", "incoming");
         chatbox.appendChild(incomingChatLi);
         chatbox.scrollTo(0, chatbox.scrollHeight);
-        generateResponse(incomingChatLi);
+        // generateResponse(incomingChatLi);
     }, 600);
     chatInput.value = "";
 }
@@ -48,22 +56,22 @@ closeBtn.addEventListener("click", () => {
     document.body.classList.remove("show-chatbot");
     chatbotToggler.style.opacity = "1";
 });
+
 chatbotToggler.addEventListener("click", () => {
     document.body.classList.add("show-chatbot");
     chatbotToggler.style.opacity = "0";
 });
 
-
 maximiseBtn.addEventListener("click", () => {
     document.body.classList.add("fullscreen");
     maximiseBtn.style.opacity = "0";
     minimiseBtn.style.opacity = "1";
-    chatBox.scrollTo(0, chatBox.scrollHeight);
+    chatbox.scrollTo(0, chatbox.scrollHeight + inputInitHeight);
 });
 
 minimiseBtn.addEventListener("click", () => {
     document.body.classList.remove("fullscreen");
     minimiseBtn.style.opacity = "0";
     maximiseBtn.style.opacity = "1";
-    chatBox.scrollTo(0, chatBox.scrollHeight);
+    chatbox.scrollTo(0, chatbox.scrollHeight + inputInitHeight);
 });
